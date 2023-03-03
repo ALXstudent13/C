121232be86e841bf7499e6f48c6a2906af8e113c
RedOne <redboukri@gmail.com>
2023-03-03 16:49:45 +0100

    Minimize Code

--- minToSec.c
+++ minToSec.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <cs50.h>
 
-int main () 
+int main(void) 
 {
-    printf ("Minutes to Seconads Converter!\n");
-    int minutes = get_int("Type Minutes: ");
+	int minute;
+	do 
+	{
+	    printf("Minutes to Seconds Converter!\n");
+       	    printf("Type Minute: ");
+	   scanf("%d",&minute);
+	}
+	 while(minute < 0);
+        
+	 int x = minute * 60 ;
 
-        if (minutes > 0)
-            {
-                int x = (minutes * 60);
-                printf ("%d minutes is equal %d secaonds!\n", minutes, x);
-            }
-        else if (minutes < 0 || minutes == 0 )
-            {
-                printf ("Pleas type real number!\n");
-            }
-        
-        
+	printf("%d minute is equal %d second",minute , x);
 }
